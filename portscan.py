9de8dd82362a80d83236112c7d24a68b15746b9b
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2022-01-30 06:53:42 +0300

    final_Version

--- portscan.py
+++ portscan.py
@@ -5,16 +5,22 @@ site = input("Hedef: ")
 target = socket.gethostbyname(site)
 
 print("-"*50)
-print("Tarama baslatildi." + target)
+print("Tarama baslatildi: " + target)
 print("-"*50)
-
+1024
 try:
-    for port in range(1, 1024):
+    x = []
+    for port in range(1,54):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         result = s.connect_ex((target, port))
         if result == 0:
-            print("Port {} Açık".format(port))
+            a =("Port {} Açık".format(port))
+            x.append(str(a) + "\n")
+            print(a)
         s.close()
+    with open("openports.txt","w",encoding="utf-8") as saved:
+        saved.writelines(site + "\n"+"Açık Portlar:"+ "\n")
+        saved.writelines(x)
 except Exception as e:
     print("HATA!", e)
